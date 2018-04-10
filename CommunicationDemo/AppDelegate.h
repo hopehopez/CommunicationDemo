60a37548ddeeb3a3313ee65089e3375767eb4de6
zhangshuqing <zhangshuqing912@163.com>
2018-04-10 12:27:27 +0800

    Initial Commit

---
+++ CommunicationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CommunicationDemo
+//
+//  Created by zsq on 2018/4/10.
+//  Copyright © 2018年 zsq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
